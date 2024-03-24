5a0fe7f84c2a7d68cc24a90da0e92b176885c2b5
inspire-lts <1183132919@qq.com>
2024-03-24 22:02:22 +0800

    feat:简易fiber架构

--- core/React.jsx
+++ core/React.jsx
@@ -1,5 +1,4 @@
 const createElement = (type, props, ...children) => {
-  console.log("调用了");
   return {
     type,
     props: {
@@ -24,23 +23,84 @@ const createTextNode = (nodeValue) => ({
 });
 
 function render(el, container) {
-  const dom =
-    el.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(el.type);
+  newWorkOfUnit = {
+    dom: container,
+    props: {
+      children: [el],
+    },
+  };
+}
 
-  Object.keys(el.props).forEach((key) => {
+let newWorkOfUnit = null;
+function workLoop(deadline) {
+  let showYield = false;
+
+  while (!showYield && newWorkOfUnit) {
+    newWorkOfUnit = performWorkOfUnit(newWorkOfUnit);
+    showYield = deadline.timeRemaining() < 1;
+  }
+
+  requestIdleCallback(workLoop);
+}
+
+function updateProps(dom, props) {
+  Object.keys(props).forEach((key) => {
     if (key !== "children") {
-      dom[key] = el.props[key];
+      dom[key] = props[key];
     }
   });
-
-  const children = el.props.children;
-  children.forEach((child) => {
-    render(child, dom);
-  });
-  container.append(dom);
 }
+
+function createDom(type) {
+  return type === "TEXT_ELEMENT"
+    ? document.createTextNode("")
+    : document.createElement(type);
+}
+
+function initChild(fiber) {
+  const children = fiber.props.children;
+  let prevChild = null;
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: fiber,
+      sibling: null,
+      dom: null,
+    };
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevChild.sibling = newFiber;
+    }
+    prevChild = newFiber;
+  });
+}
+function performWorkOfUnit(fiber) {
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type));
+
+    fiber.parent.dom.append(dom);
+
+    updateProps(dom, fiber.props);
+  }
+
+  // 3. 转换链表，设置指针
+  initChild(fiber);
+
+  // 4. 返回下一个要执行的任务
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  if (fiber.sibling) {
+    return fiber.sibling;
+  }
+
+  return fiber.parent?.sibling;
+}
+requestIdleCallback(workLoop);
 
 const React = {
   render,
